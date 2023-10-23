eba6131d9edff4cd3521548774b6cd224ac8903b
Nichole Zacherl <nichole.zacherl@gmail.com>
2023-10-23 15:59:10 -0400

    Added react project and controller imlementations

---
+++ MyGardenPlanner/Controllers/PlantController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyGardenPlanner.Data;
+using MyGardenPlanner.Models;
+
+namespace MyGardenPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantController : ControllerBase
+    {
+        private readonly GardenContext _context;
+        public PlantController(GardenContext gardenContext)
+        {
+            _context = gardenContext;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Plant>> Get()
+        {
+            return _context.Plant;
+        }
+    }
+}
---
+++ reactapp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+
+        return (
+            <div>
+                <div> hello </div>
+            </div>
+        );
+    }
+}
